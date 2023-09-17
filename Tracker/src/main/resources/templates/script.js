93dfade07576df0cad5cd8b146fcc7e746b838d3
Gabriel <murariugabi23@gmail.com>
2023-09-17 12:02:34 +0300

    Application now loads data from the API

--- Tracker/src/main/resources/templates/script.js
+++ Tracker/src/main/resources/templates/script.js
@@ -1,6 +1,7 @@
-//Accessing DB function
+                    //Accessing DB function
 
 const url = "http://localhost:8080/categories"
+const url2 = "http://localhost:8080/transactions"
 
 const getData = async (url) => {
 
@@ -10,6 +11,8 @@ const getData = async (url) => {
 
         console.log(data)
 
+        return data
+
     } catch (error) {
         
     }
@@ -17,6 +20,95 @@ const getData = async (url) => {
 }
 
 // getData(url)
+getData(url2)
+
+
+                    // Loading application functionality
+
+const categoryFilters = document.getElementById("category_filter")
+const transactions = document.body.querySelector(".transactions_container")
+const categoriesList = document.getElementById("categories_list")
+
+
+// Scroll functionality for categories - IMPORTANT!!! 
+
+// SIMPLEBAR CONTAINER LOADS BEFORE I GET TO FETCH THE CATEGORIES SO THAT IS WHY I AM ABLE TO JUST APPEND THEM
+const scrollbar = new SimpleBar(document.getElementById("categories_list"))
+const simpleBarContainer = document.body.querySelector(".simplebar-content")
+
+
+
+// Template functions for loading certain elements
+
+const filterOption = (args) => {
+    const {description} = args
+
+    const option = document.createElement("option")
+    option.value = description
+    option.innerText = description
+
+   return option
+}
+
+const transaction = (args) => {
+
+    const {date, details, amount, category} = args
+
+    const listElement = document.createElement("li")
+    listElement.innerHTML = 
+    `
+    <span>${date}</span>
+    <span>${details}</span>
+    <span>${category.description}</span>
+    <span>$ ${amount}</span>
+    `
+
+    return listElement
+}
+
+const categoryElement = (args) => {
+    const {description, type} = args
+    
+    const listElement = document.createElement("li")
+    listElement.innerHTML = 
+    `
+    <span>${description}</span>
+    <span>${type.toLowerCase()}</span>
+    `
+  
+    return listElement
+}
+
+
+
+// Load function
+
+const loadElements = (parent, url, childTemplate,) => {
+    
+    getData(url).then(data => {
+        data.map(element => {
+            const { ...args } = element
+            const child = childTemplate(args)
+            parent.appendChild(child)
+        })
+    })
+    
+    
+}
+
+
+
+
+
+loadElements(categoryFilters, url, filterOption)
+loadElements(transactions,url2, transaction)
+loadElements(simpleBarContainer, url, categoryElement)
+
+
+
+
+
+
 
 // here I will need to make a function that loops through the transactions that need to be displayed and append them - if you need reference you have it is some old folders 
 
@@ -66,7 +158,3 @@ categoriesTrigger.addEventListener("click", () => {
 })
 
 
-
-// Scroll functionality for categories list 
-const container = document.querySelector("[data-simplebar]")
-const scrollbar = new SimpleBar(container)
